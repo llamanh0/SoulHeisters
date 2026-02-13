ee1621cf83cae70ca959b275112c9e54f51553fe
H. Batuhan Kılıçkan <146671954+llamanh0@users.noreply.github.com>
2026-02-13 17:07:02 +0300

    Basic Network and Movement Added

--- SoulHeisters_clone_0/Assets/_Project/Scripts/Player/PlayerInputHandler.cs
+++ SoulHeisters_clone_0/Assets/_Project/Scripts/Player/PlayerInputHandler.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // (Encapsulation)
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool IsJumping { get; private set; }
+    public bool IsSprinting { get; private set; }
+
+    private PlayerInputActions _inputActions;
+
+    private void OnEnable()
     {
-        
+        if (_inputActions == null)
+        {
+            _inputActions = new PlayerInputActions();
+
+            // WASD
+            _inputActions.Player.Move.performed += i => MoveInput = i.ReadValue<Vector2>();
+            _inputActions.Player.Move.canceled += i => MoveInput = Vector2.zero;
+
+            // Look
+            _inputActions.Player.Look.performed += i => LookInput = i.ReadValue<Vector2>();
+            _inputActions.Player.Look.canceled += i => LookInput = Vector2.zero;
+
+            // Space
+            _inputActions.Player.Jump.performed += i => IsJumping = true;
+            _inputActions.Player.Jump.canceled += i => IsJumping = false;
+
+            // Shift
+            _inputActions.Player.Sprint.performed += i => IsSprinting = true;
+            _inputActions.Player.Sprint.canceled += i => IsSprinting = false;
+        }
+
+        _inputActions.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _inputActions?.Disable();
     }
 }
