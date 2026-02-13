e0ad90d20824c27309bb1b561bc25c45a15a5acb
H. Batuhan Kılıçkan <146671954+llamanh0@users.noreply.github.com>
2026-02-13 15:16:10 +0300

    Initial commit

---
+++ SoulHeisters/Assets/_Project/Scripts/Core/AppNetManager.cs
@@ -0,0 +1,72 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class AppNetManager : MonoBehaviour
+{
+    public static AppNetManager Instance { get;  private set; }
+
+    private void Awake()
+    {
+        if (Instance && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        if(NetworkManager.Singleton != null)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += HandleClientDisconnected;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (NetworkManager.Singleton != null)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback -= HandleClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= HandleClientDisconnected;
+        }
+    }
+
+    private void HandleClientDisconnected(ulong clientId)
+    {
+        Debug.Log($"[AppNetManager] Connected Player => ID: {clientId}");
+
+        // If connected player is the LocalPlayer => Success
+        if (clientId == NetworkManager.Singleton.LocalClientId) 
+        { 
+            Debug.Log("[AppNetManager] You Connected Successfully!");
+        }
+    }
+
+    private void HandleClientConnected(ulong clientId)
+    {
+        Debug.Log($"[AppNetManager] Disconnected Player => ID: {clientId}");
+
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            Debug.Log("[AppNetManager] You Disconnected Successfully!");
+
+            // WILL ADD: Return to Main Menu
+        }
+    }
+
+    // Connection Functions
+    public void StartHost()
+    {
+        Debug.Log("Host is being start.");
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        Debug.Log("Client is being start.");
+        NetworkManager.Singleton.StartClient();
+    }
+}
