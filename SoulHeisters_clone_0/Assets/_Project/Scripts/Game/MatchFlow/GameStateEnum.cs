f5921231d5b8d30ccc3fad92411a5658e231eeca
H. Batuhan Kılıçkan <146671954+llamanh0@users.noreply.github.com>
2026-05-30 22:34:28 +0300

    ParrelSync Removed

--- SoulHeisters_clone_0/Assets/_Project/Scripts/Game/MatchFlow/GameStateEnum.cs
+++
@@ -1,8 +0,0 @@
-public enum GameState
-{
-    WaitingForPlayers,
-    Starting,
-    Playing,
-    ExtractionPhase,
-    MatchEnded
-}
--- SoulHeisters_clone_0/Assets/_Project/Scripts/Player/Spells/Enums/SpellCastResultEnum.cs
+++
@@ -1,9 +0,0 @@
-/// <summary>
-/// Bir spell cast girisiminin sonucu.
-/// </summary>
-public enum SpellCastResult
-{
-    Success,
-    NotEnoughMana,
-    OnCooldown
-}
